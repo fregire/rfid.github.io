6915edcb036d64ed32b18a8f1612bd96f368335e
fregire <fregire@mail.ru>
2018-09-26 17:49:21 +0500

    Актиаировал слайдер для работ + появление брелка

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$(".page-header__brelok").delay(1000).addClass("page-header__brelok--visible");
+
+	// Slick - слайдеры
+
+	// Слайдер с работами
+	$(".projects__item").slick({
+		fade: true,
+		autoplay: true,
+  		autoplaySpeed: 5000,
+	});
+});
