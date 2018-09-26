c6f1806e1d49b4eef855f62ac573e4986e629b8d
fregire <fregire@mail.ru>
2018-09-26 18:38:13 +0500

    Параллакс - эффект для блока с производством

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 
 $(document).ready(function(){
+
+	// Появление брелка
 	$(".page-header__brelok").delay(5000).addClass("page-header__brelok--visible");
 
 	// Slick - слайдеры
@@ -9,5 +11,17 @@ $(document).ready(function(){
 		fade: true,
 		autoplay: true,
   		autoplaySpeed: 5000,
+	});
+
+	// Параллакс - эффект на блоке с Производством и поставкой
+	var yPos = 0;
+	var speed = parseInt($(".production").attr("data-speed"));
+	var startedPosXOfBg = -200;
+	var coords;
+
+	$(window).scroll(function(e){
+		yPos = $(window).scrollTop() /  speed;
+		var coords = -yPos + startedPosXOfBg + 'px';
+		$(".production").css({ backgroundPositionY: coords });
 	});
 });
